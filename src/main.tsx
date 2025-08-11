9dc1f45373c9901fbc5ee14c8750a8abdaa1cf66
mohamedkeliwa <mhmdalknj496@gmail.com>
2025-08-11 11:06:24 +0300

    feat: add ScrollToTop component to enhance navigation experience

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,13 @@ import MembershipCardPage from './pages/MembershipCard.tsx';
 import MembersPage from './pages/Members.tsx';
 import ProgramsPage from './pages/Programs.tsx';
 import ContactAndGroupsPage from './pages/ContactAndGroups.tsx';
+import ScrollToTop from './components/ScrollToTop.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/administrative" element={<AdministrativePage />} />
