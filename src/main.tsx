7661e2bcfe47bc66014aa23ea8fcb28008397089
mohamedkeliwa <mhmdalknj496@gmail.com>
2025-09-26 00:46:22 +0300

    feat: add @vercel/analytics for enhanced tracking and analytics capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import MembersPage from './pages/Members.tsx';
 import ProgramsPage from './pages/Programs.tsx';
 import ContactAndGroupsPage from './pages/ContactAndGroups.tsx';
 import ScrollToTop from './components/ScrollToTop.tsx';
+import { Analytics } from "@vercel/analytics/react";
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/programs" element={<ProgramsPage />} />
         <Route path="/contact" element={<ContactAndGroupsPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
